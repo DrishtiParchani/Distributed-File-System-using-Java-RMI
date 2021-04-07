8bd84e9ebd61cfe73578c2f580d787774e3f4138
DrishtiParchani <drishtip99@gmail.com>
2021-04-07 14:42:34 +0530

    Add files via upload

---
+++ FileInfo.java
@@ -0,0 +1,8 @@
+
+/* by Jia Rao */
+import java.io.Serializable;
+public interface FileInfo extends Serializable {
+	String getName();
+	byte[] getContent();
+	void setInfo(String name, byte[] content);
+}
